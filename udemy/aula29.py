41f0cf580fae3df5decb20ba9f02766652ba036f
André Finco <62232432+AndreRyok@users.noreply.github.com>
2024-11-14 12:08:32 -0300

    att

--- udemy/aula29.py
+++ udemy/aula29.py
@@ -2,4 +2,7 @@ nome = 'André Finco'
 altura = 1.86
 peso = 93
 imc = peso / (altura ** 2)
-print(f'{nome} tem {altura} de altura, pesa {peso}kg e seu IMC é {imc}')
+
+linha_1 = f'{nome} tem {altura} de altura, pesa {peso}kg e seu IMC é {imc:.2f}' 
+
+print(linha_1)
