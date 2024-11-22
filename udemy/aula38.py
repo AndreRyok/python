b44067e48acb0a1223a25b748b5047a0bc3b7de1
André Finco <62232432+AndreRyok@users.noreply.github.com>
2024-11-22 12:21:33 -0300

    att

---
+++ udemy/aula38.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'primeiro_valor = {primeiro_valor} é maior que segundo_valor = {segundo_valor}')
+else:
+    print(f'segundo_valor = {segundo_valor} é maior que primeiro_valor = {primeiro_valor}')
---
+++ udemy/aula43.py
@@ -0,0 +1,10 @@
+# Operadores in e not in
+# Strings são iteráveis
+
+#  0 1 2 3 4
+#  A n d r é
+# -5-4-3-2-1
+
+nome = 'André'
+
+print('é' in nome)
