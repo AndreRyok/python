750df84e16ffbfc2fd1581059a50c9987a4b0568
AndreRyok <62232432+AndreRyok@users.noreply.github.com>
2024-11-24 11:35:08 -0300

    att

---
+++ udemy/aula44.py
@@ -0,0 +1,5 @@
+nome = 'André'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %04x' % (15, 15))
---
+++ udemy/aula46.py
@@ -0,0 +1,11 @@
+"""
+ 012345678
+ Olá mundo
+-987654321
+
+Fatiamento [i:f:p] [::]   --->  inínio / fim / passo
+Obs: a função len retorna a quantidade de caracteres da str
+"""
+
+variavel = 'Olá mundo'
+print(variavel[::-1])
