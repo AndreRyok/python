23760daafd990a8c6d6ca667c61f2072a758aee8
André Finco <62232432+AndreRyok@users.noreply.github.com>
2024-11-28 11:58:30 -0300

    att

---
+++ udemy/aula52.py
@@ -0,0 +1,12 @@
+"""
+Flag (bandeira) - marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor e identidade) 
+id = identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+
+print(id(v1))
+print(id(v2))
---
+++ udemy/aula53.py
@@ -0,0 +1,16 @@
+"""
+Flag (bandeira) - marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor e identidade) 
+id = identidade
+"""
+
+condicao = False
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+print(passou_no_if)
