3c606cc38f6d0d200591422082be070d5ba72488
AndreRyok <62232432+AndreRyok@users.noreply.github.com>
2024-12-11 15:38:00 -0300

    att

--- udemy/aula59.py
+++ udemy/aula59.py
@@ -1,5 +1,16 @@
 contador = 0
 
-while contador < 10:
+while contador < 100:
     contador += 1
-    print(contador)X
+
+    if contador == 6:
+        print('Não vou mostrar o 6')
+        continue    
+
+    if contador >= 10 and contador <= 27:
+        continue
+
+    print(contador)
+
+    if contador == 40:
+        break
