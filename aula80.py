f9ef8aa58cc72fe1b0a5f1825b864e3c4a9bf011
AndreRyok <62232432+AndreRyok@users.noreply.github.com>
2025-01-09 11:57:36 -0300

    att

---
+++ aula80.py
@@ -0,0 +1,8 @@
+string = 'ABCDE'
+
+#         0     1      2      3    4
+#        -5    -4     -3     -2   -1
+lista = [123, True, 'André', 1.2, []]
+lista[-3] = 'Anna Clara'
+
+print(lista[2], type(lista[2]))
---
+++ udemy/aula81.py
@@ -0,0 +1,6 @@
+lista = [10, 20, 30, 40]
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append(70)
+print(lista)
