16037f3b253fb53e83072acd498edd64dfd01427
André Finco <62232432+AndreRyok@users.noreply.github.com>
2025-02-05 12:02:22 -0300

    att

---
+++ udemy/aula94.py
@@ -0,0 +1,16 @@
+# Listas de listas e seus índices
+
+salas = [
+    #   0         1
+    ['Maria', 'Helena'],               # 0
+
+    #   0  
+    ['Elaine'],                        # 1
+
+    #   0       1        2               3
+    ['Luiz', 'João', 'Eduarda']       # 2
+]
+
+for sala in salas:
+    for aluno in sala:
+        print(aluno)
